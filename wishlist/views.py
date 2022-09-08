ddb936ca70125493a9a46e24cd30025570d13f63
Adam Wilson Autan <adam.wilson@ui.ac.id>
2022-09-08 13:31:36 +0700

    Submisi 2 Lab 1

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from wishlist.models import BarangWishlist
 
 # Create your views here.
 
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Adam'
+}
+
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    return render(request, "wishlist.html", context)
